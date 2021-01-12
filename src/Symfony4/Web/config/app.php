a0e69eea6f6542b34ec5e502eb2e9b60549aed5f
Vitaliy <theyamshikov@yandex.ru>
2021-01-12 17:03:07 +0600

    upd

--- src/Symfony4/Web/config/app.php
+++ src/Symfony4/Web/config/app.php
@@ -4,6 +4,7 @@ use Illuminate\Container\Container;
 use ZnCore\Base\Libs\DotEnv\DotEnv;
 use ZnLib\Init\Symfony4\Web\InitModule;
 use ZnLib\Web\Symfony4\MicroApp\MicroApp;
+use ZnCore\Base\Helpers\EnvHelper;
 
 $rootDir = realpath(__DIR__ . '/../../../../../../..');
 require_once $rootDir . '/common/Bootstrap/autoload.php';
@@ -12,7 +13,7 @@ DotEnv::init($rootDir);
 $container = Container::getInstance();
 
 $app = new MicroApp($container);
-$app->setErrorLevel(E_ALL);
+EnvHelper::showErrors();
 $app->addModule(new InitModule);
 $response = $app->run();
 $response->send();
